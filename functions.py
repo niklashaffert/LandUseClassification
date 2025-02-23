cff20d9f6d11c743ecb627a273b840606ac56363
Niklas Haffert <50962410+niklashaffert@users.noreply.github.com>
2025-02-23 19:16:38 +0100

    add read_drive

--- functions.py
+++ functions.py
@@ -2,3 +2,10 @@
 def store_drive(object, path):
   with open(path, 'wb') as f:
       dump(object, f)
+
+@staticmethod
+def read_drive(path):
+  with open(path, 'rb') as f:
+      obj = load(f)
+  return obj
+     
